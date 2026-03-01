36d6a48e4e9f3c91391ca52515053360f8046e43
cristy-mariya <cristymariya16@gmail.com>
2026-03-01 12:19:23 +0530

    new

--- src/ReverseInteger.java
+++ src/ReverseInteger.java
@@ -1,3 +1,14 @@
+import java.util.Scanner;
 public class ReverseInteger { 
-        // TODO: Read an integer and print its reverse  
+        public static void main(String[] args)
+{                Scanner sc= new Scanner(System.in);
+                 int n= sc.nextInt();
+                 int reversed=0;
+                 while(n!=0)
+{                   
+                   int a=n%10;
+                   reversed=reversed*10+a;
+                   n=n/10; }
+                 System.out.print(reversed);}
+           
 }
